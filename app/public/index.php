52ace6619a5eb3ee190eb1934ed7acb32be1c2bf
thienphuc <thienphuc484@gmail.com>
2024-11-12 09:22:11 +0700

    Update index.php

--- app/public/index.php
+++ app/public/index.php
@@ -1,21 +1,28 @@
 <?php
 namespace App\Public;
 
-use App\Config\Database;
-use Exception;
+use App\Src\Dao\Impl\UrlDaoImpl;
+use App\Src\Service\Impl\UrlServiceImpl;
+use App\Src\Controller\URLController;
+
 require_once __DIR__ . '/../Config/database.php';
+require_once __DIR__ .'/../src/dao/impl/UrlDaoImpl.php';
+require_once __DIR__ .'/../src/service/UrlService.php';
+require_once __DIR__ .'/../src/service/impl/UrlServiceImpl.php';
+require_once __DIR__ . '/../src/controller/URLController.php';
 
-try {
-    $db = Database::getInstance();
-    Database::setCharsetEncoding();
+$code = isset($_GET['code']) ? htmlspecialchars(trim($_GET['code'],"/"), ENT_QUOTES, 'UTF-8') : null;
+$origin_url = isset($_POST['origin_url']) ? filter_var($_POST['origin_url'], FILTER_SANITIZE_URL) : '';
 
-    $sql = "SELECT VERSION();";
-    $stm = $db -> prepare($sql);
+$controller = new URLController(new UrlServiceImpl(new UrlDaoImpl()));
 
-    $stm -> execute();
-    $result = $stm->fetchAll();
-    echo $result[0][0]; 
-} catch (Exception $e) {
-    echo $e->getMessage();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $controller -> create($origin_url);
+} else {
+    if ($code != null) {
+        $controller -> getOriginByCode($code);
+    } else {
+        include __DIR__ . '/../src/view/CreateUrlView.php';
+    }
 }
 ?>
